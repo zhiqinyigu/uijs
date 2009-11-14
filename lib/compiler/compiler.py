480d5c071fbd9ed6b4a4599925ef2ce1daf6b4af
lvxuhui@gmail.com <lvxuhui@gmail.com@8c8ed0d4-9db2-11de-a782-b75b60b233d1>
2009-11-14 14:06:43 +0000

    拆出UI.Flash,UI.Gotop,UI.Resize

--- lib/compiler/compiler.py
+++ lib/compiler/compiler.py
@@ -10,8 +10,11 @@ java -jar compiler.jar --js=../ui.Dialog.js --js_output_file=ui.tmp.js
  --js=../ui.Dialog.js
  --js=../ui.DragDrop.js
  --js=../ui.DropMenu.js
+ --js=../ui.Flash.js
+ --js=../ui.Gotop.js
  --js=../ui.Menu.js
  --js=../ui.PopupMenu.js
+ --js=../ui.Resize.js
  --js=../ui.Select.js
  --js=../ui.SelectMulti.js
  --js=../ui.ShowBar.js
